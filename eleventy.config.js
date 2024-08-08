b3be89bb8cfa276d9f8a321fd24f42185824cc93
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-08-08 13:17:56 -0500

    Switch to WebC

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,9 @@
+import webC from "@11ty/eleventy-plugin-webc"
+
 export default function(eleventyConfig) {
-	eleventyConfig.setInputDirectory('page')
-	eleventyConfig.setOutputDirectory('_site')
+	eleventyConfig.addPlugin(webC)
+	eleventyConfig.ignores.add("readme.md")
 	eleventyConfig.setFrontMatterParsingOptions({ language: 'js' })
+	eleventyConfig.setInputDirectory('index.webc')
+	eleventyConfig.setOutputDirectory('_site')
 }
