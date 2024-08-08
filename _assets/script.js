02167ae598e35d522b7dd6ffa6a2f510c1a87807
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-08-08 16:53:52 -0500

    Rough out query intercept

--- _assets/script.js
+++ _assets/script.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!')
+const searchForm = document.getElementById('search').form
+
+const constructQuery = (event) => {
+	event.preventDefault()
+
+	const searchValue = event.target.search.value
+
+	console.log(`Search for “${searchValue}”`)
+}
+
+searchForm.onsubmit = constructQuery
