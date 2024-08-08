19b30651d771a2cd02e21fa061a5876006c128e6
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-08-08 17:27:20 -0500

    Proof-of-concept

--- _assets/script.js
+++ _assets/script.js
@@ -3,9 +3,11 @@ const searchForm = document.getElementById('search').form
 const constructQuery = (event) => {
 	event.preventDefault()
 
+	const baseUrl = 'https://provenance.dev.getty.fargeo.com/search'
 	const searchValue = event.target.search.value
+	const searchParams = `?term-filter=[{"inverted":false,"type":"string","context":"","context_label":"","id":"${searchValue}","text":"${searchValue}","value":"${searchValue}","selected":true}]&language=*`
 
-	console.log(`Search for “${searchValue}”`)
+	window.location.href = baseUrl + searchParams
 }
 
 searchForm.onsubmit = constructQuery
