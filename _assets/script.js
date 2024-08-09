3a60fe0b3076c0f1d2480ac3f51b6a9829edd21f
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-08-09 16:59:45 -0500

    Total record count (client-side)

--- _assets/script.js
+++ _assets/script.js
@@ -8,3 +8,25 @@ searchForm.onsubmit = (event) => {
 	const searchParams = `?term-filter=[{"inverted":false,"type":"string","context":"","context_label":"","id":"${searchValue}","text":"${searchValue}","value":"${searchValue}","selected":true}]&language=*`
 	window.location.href = searchUrl + searchParams
 }
+
+const apiHeading = document.querySelector('h2[data-api-url')
+const apiUrl = apiHeading.dataset.apiUrl
+delete apiHeading.dataset.apiUrl
+
+const sparqlQuery = `
+	SELECT (COUNT(DISTINCT ?s) AS ?totalRecords) WHERE {
+		?s ?p ?o.
+	}
+`
+
+fetch(`${apiUrl}?query=${encodeURIComponent(sparqlQuery)}&format=application/sparql-results+json`)
+	.then(response => response.json())
+	.then(data => {
+		const totalRecords = Number(data.results.bindings[0].totalRecords.value).toLocaleString()
+		const countElement = `
+			<p>${totalRecords} records</p>
+		`
+
+		apiHeading.insertAdjacentHTML('afterend', countElement)
+	})
+	.catch(error => console.error('Error:', error))
