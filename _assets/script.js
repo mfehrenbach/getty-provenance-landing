f2fd3f39c962347ea71aa359f7600661ba8ab68a
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-08-09 11:44:24 -0500

    Tidy JS

--- _assets/script.js
+++ _assets/script.js
@@ -2,13 +2,9 @@ const searchForm = document.getElementById('search').form
 const searchUrl = searchForm.dataset.searchUrl
 delete searchForm.dataset.searchUrl
 
-const constructQuery = (event) => {
+searchForm.onsubmit = (event) => {
 	event.preventDefault()
-
 	const searchValue = event.target.search.value
 	const searchParams = `?term-filter=[{"inverted":false,"type":"string","context":"","context_label":"","id":"${searchValue}","text":"${searchValue}","value":"${searchValue}","selected":true}]&language=*`
-
 	window.location.href = searchUrl + searchParams
 }
-
-searchForm.onsubmit = constructQuery
