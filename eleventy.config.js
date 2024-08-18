9eda5517573a2fb2a824bb2ecf673daf9667a291
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-08-18 16:01:52 -0400

    Light post-build formatting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,24 @@
-import webC from "@11ty/eleventy-plugin-webc"
+import webC from '@11ty/eleventy-plugin-webc'
+import jsBeautify from 'js-beautify'
 
 export default function(eleventyConfig) {
 	eleventyConfig.addPlugin(webC)
-	eleventyConfig.ignores.add("readme.md")
+	eleventyConfig.ignores.add('readme.md')
 	eleventyConfig.setFrontMatterParsingOptions({ language: 'js' })
 	eleventyConfig.setInputDirectory('index.webc')
 	eleventyConfig.setOutputDirectory('_site')
+
+	const beautifyOptions = {
+		"break_chained_methods": true,
+		"extra_liners": [],
+		"indent_empty_lines": false,
+		"indent_inner_html": true,
+		"indent_with_tabs": true,
+		"preserve_newlines": false,
+		"selector_separator_newline": true,
+		"space_around_combinator": true,
+		"wrap_line_length": 0,
+	}
+
+	eleventyConfig.addTransform('beautify', async (content) => jsBeautify.html(content, beautifyOptions))
 }
