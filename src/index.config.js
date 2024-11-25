a5b614f843c050ea3d9ecf93b629974786a19e1d
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-11-25 12:15:53 -0800

    Navigation data/DOM

--- src/index.config.js
+++ src/index.config.js
@@ -1,21 +1,39 @@
 export default {
-	title:    'Getty Provenance Index',
-	baseUrl:  'https://provenance.dev.getty.fargeo.com',
-	apiUrl:   'https://data.getty.edu/provenance',
+	title:   'Getty Provenance Index',
+	baseUrl: 'https://provenance.dev.getty.fargeo.com',
+	apiUrl:  'https://data.getty.edu/provenance',
 	breadcrumb: {
 		title: 'Getty Homepage',
 		url:   'https://www.getty.edu',
 		crumbs: [
 			{
 				title: 'Research & Conservation',
-				url:  '/research-conservation',
+				url:   '/research-conservation',
 			},
 			{
 				title: 'Databases, Tools & Technologies',
-				url:  '/research/tools',
+				url:   '/research/tools',
 			},
 		],
 	},
+	navigation: [
+		{
+			title: 'About the Index',
+			url:   '/research/tools/provenance/',
+		},
+		{
+			title: 'What’s Covered?',
+			url:   '/research/tools/provenance/charts.html',
+		},
+		{
+			title: 'Remodel Project',
+			url:   '/research/tools/provenance/provenance_remodel',
+		},
+		{
+			title: 'FAQ',
+			url:   '/research/tools/provenance/faq.html#faq',
+		},
+	],
 	search:  {
 		label:  'Search 2.3 million records…:',
 		button: 'Search',
@@ -25,7 +43,7 @@ export default {
 		examples: [
 			{
 				title: 'Vincent van Gogh',
-				url:  '/report/8042aa22-d6ad-310f-9ec6-f1467b24d593',
+				url:   '/report/8042aa22-d6ad-310f-9ec6-f1467b24d593',
 			},
 		],
 	},
