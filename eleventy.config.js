c9ba048823c2f5502849a367df186112bdac7817
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-11-26 22:13:20 -0800

    Use `addTransform` for inlining the fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,21 +9,27 @@ export default function(eleventyConfig) {
 	eleventyConfig.setInputDirectory('src/index.webc')
 	eleventyConfig.setOutputDirectory('public')
 
-	// For inlining assets.
-	eleventyConfig.addLiquidShortcode('base64FromFile', async (file) => Buffer.from(await fs.promises.readFile(file)).toString('base64'))
-
 	// Do some post-build formatting.
 	const beautifyOptions = {
-		break_chained_methods: true,
-		extra_liners: [],
-		indent_empty_lines: false,
-		indent_inner_html: true,
-		indent_with_tabs: true,
-		preserve_newlines: false,
+		break_chained_methods:      true,
+		extra_liners:               [],
+		indent_empty_lines:         false,
+		indent_inner_html:          true,
+		indent_with_tabs:           true,
+		preserve_newlines:          false,
 		selector_separator_newline: true,
-		space_around_combinator: true,
-		wrap_line_length: 0,
+		space_around_combinator:    true,
+		wrap_line_length:           0,
 	}
 
 	eleventyConfig.addTransform('beautify', async (content) => jsBeautify.html(content, beautifyOptions))
+
+	// And inline the font files.
+	eleventyConfig.addTransform('base64FontFile', async (content) => {
+		for (const match of content.matchAll(/base64,\{([^}]+)\}/g)) {
+			content = content.replace(match[0], `base64,${(await fs.promises.readFile(match[1])).toString('base64')}`)
+		}
+
+		return content
+	})
 }
