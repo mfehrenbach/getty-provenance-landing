e0e535a5c8621d7ce5c413037b170eedbf918bd3
Michael Fehrenbach <mf@michaelfehrenbach.com>
2024-12-05 00:52:52 -0500

    Whole bunch of examples

--- src/index.config.js
+++ src/index.config.js
@@ -146,6 +146,12 @@ export default {
 				report: '0f97dcdc-020e-345d-9c14-4ec4d4ad913e',
 			},
 			{
+				// Géricault’s “Race of the Riderless Horses.”
+				object: '26f763e1-078d-4168-ba1b-e074e0d24540',
+				image:  '8fe76ec0-34e8-436e-82b8-a831e7fbc7ac',
+				report: '0f97dcdc-020e-345d-9c14-4ec4d4ad913e',
+			},
+			{
 				// Poussin’s “Landscape with a Calm.”
 				object: '9f3d6258-9711-41a8-8d1c-640c39289691',
 				image:  '769f2327-0618-4c6c-ad32-7035dd763f26',
@@ -174,6 +180,126 @@ export default {
 				object: '9e4be67e-e28d-4bd3-a026-3ad52411f61a',
 				image:  'd80d5a81-edda-4650-9c5b-c3449513552c',
 				report: '47a6a315-0cf1-35c1-97ee-e7081703eef9',
+			},
+			{
+				// Bouts’ “The Annunciation.”
+				object: 'bd596097-af83-44f7-9830-955dbd868279',
+				image:  'b02a1200-932c-4cc6-b29c-9daddf40e32c',
+				report: 'f425e5e6-8805-32fa-9d2f-8602010dd524',
+			},
+			{
+				// Bouguereau’s “Young Girl Defending Herself against Love.”
+				object: '83b4d6a0-9e9b-4660-a9bc-74efb0f7a5c4',
+				image:  '4519972a-b71c-4445-9d19-feb9d186d32e',
+				report: 'ef03cc8f-1c6b-3848-8b63-e4c27ef22093',
+			},
+			{
+				// Tissot’s “Portrait of the Marquise de Miramon.”
+				object: '8d067b34-64d9-4219-8abb-ca18a7336b0e',
+				image:  '7f951529-a966-453b-ab1f-2f960b7a565e',
+				report: '50dabfb7-ad46-3f51-bf36-91c08b320795',
+			},
+			{
+				// Lorrain’s “Coast View with the Abduction of Europa.”
+				object: '01e5106c-a924-4e6e-8c90-6115f4a6a26d',
+				image:  'd18a18fd-5c58-4b19-a479-cf445606c1d7',
+				report: 'e8afe6f1-94aa-3b9e-bdd6-0096ea21ae63',
+			},
+			{
+				// Renoir’s “Promenade.”
+				object: '3c96c8a9-2f86-4384-9d56-d0423600c651',
+				image:  '7361d7d9-3907-40f7-b487-d0378c107ef6',
+				report: '4cf790d0-1631-3178-a0c3-c414e07041b9',
+			},
+			{
+				// Rembrandt’s “Saint Bartholomew.”
+				object: '5de8cebd-36f7-4a33-8774-6ef21ca5da7b',
+				image:  'a21fe655-2d17-441c-b224-723041db18b9',
+				report: 'dde29a68-fea4-3f3d-89b2-dbc0688a48ae',
+			},
+			{
+				// Monet’s Portal of Rouen Cathedral.”
+				object: '182385f7-61c4-4e2b-a815-db26722409f9',
+				image:  '286d361b-8da7-4a0a-925f-4ab350e605f5',
+				report: '1d44a590-d899-34fe-b866-b2b303d34706',
+			},
+			{
+				// Watteau’s “La Surprise.”
+				object: '97dde16c-6e22-46f0-848a-879acfc78271',
+				image:  'c145ef8f-d59b-4422-b695-8c9388cbf848',
+				report: '72b3c586-3588-3799-be34-f6ad3c235dfd',
+			},
+			{
+				// Segantini’s “Spring in the Alps.”
+				object: '3eb47da8-67c8-4932-9308-eb16924067be',
+				image:  '719850ea-ca08-4c7d-8d90-542075acd37d',
+				report: 'dfbe6a15-9257-307b-b237-2e9906b3b077',
+			},
+			{
+				// Robert’s “Demolition of the Château of Meudon.”
+				object: '4f3a72f0-99a1-4361-8715-7a29a6ec7eb8',
+				image:  'ed31e38d-068f-41a7-923f-d6daaa174ce7',
+				report: 'f9f357bf-c492-3612-b16c-a0ec2d89b4ef',
+			},
+			{
+				// Bassano’s “Miracle of the Quails.”
+				object: '4e288fcd-e1c0-4150-b202-131dc3463f24',
+				image:  '968c1b0c-9b1d-4526-827e-588437a5ccde',
+				report: '5fb5c4fe-07df-327f-8ef7-75f6f61752bf',
+			},
+			{
+				// Manet’s “Jeanne.”
+				object: '951295b7-dfb2-42fa-b06e-5a1c350dd88d',
+				image:  '0bd92353-059c-466c-87ab-00086c02304e',
+				report: '8daf18af-f59c-313c-ae9f-80d4e01fbc81',
+			},
+			{
+				// Pontormo’s “Portrait of a Halberdier.”
+				object: '56016db9-20a4-4a99-814b-23ac542a106e',
+				image:  '972b5d89-aa07-4572-9075-c82f520fd1fb',
+				report: '05a80c5b-5a5f-37a8-9456-bba39a54e28c',
+			},
+			{
+				// Bastida’s “Hall of the Ambassadors.”
+				object: '2893d02b-a442-4ee3-a789-2af6360560f3',
+				image:  '798f6d57-f0a8-46be-84b3-2544caa56c44',
+				report: 'c44c2286-ae0b-335d-90c7-09278e9985e1',
+			},
+			{
+				// Mengs’ “Portrait of José Nicolás de Azara.”
+				object: 'b856d28e-e76a-4b46-b570-ec3be93090c9',
+				image:  '14ac4123-1aa6-4d2e-8301-f814d1d50ce8',
+				report: 'c5b51571-9175-3358-96a7-fa551f5516ca',
+			},
+			{
+				// Zuccaro’s “Cupid and Pan.”
+				object: 'da4ff78d-766b-4bdc-92da-118591355a2f',
+				image:  '7388a215-a603-4e08-a34a-5cb51acc9fc9',
+				report: '76c4e2c0-f235-3f5b-a77e-cf936a90a67e',
+			},
+			{
+				// Tiepolo’s “Alexander the Great and Campaspe.”
+				object: '3b76019c-f93e-4839-a696-c89cb44740c1',
+				image:  '35b5a2e5-eaba-4b97-9a58-5c73cec3fa90',
+				report: '2ac9e24e-de4c-39d4-94d7-7fc8e0b3520c',
+			},
+			{
+				// Titian’s “Portrait of Alfonso d'Avalos.”
+				object: '8224442b-7d61-4c10-bbe1-9cb62158ca88',
+				image:  'a8cc6391-5bd6-4b56-910f-7546010b3317',
+				report: 'e2c5bd6d-1129-397a-823d-eff0df08bd57',
+			},
+			{
+				// Titian’s “Penitent Magdalene.”
+				object: 'aaeb7771-929e-4bb8-b0b1-3c2f2ed13a1f',
+				image:  '6a3de87b-d5b8-46f6-a8d8-cd70bb8efc39',
+				report: 'e2c5bd6d-1129-397a-823d-eff0df08bd57',
+			},
+			{
+				// Canaletto’s “View of the Arch of Constantine.”
+				object: '8019f0d7-4736-4189-ab41-bdc8ae0b462d',
+				image:  'f70a0611-cd18-467c-97ce-f5590a9aadab',
+				report: 'c97c40a1-2cc6-39d4-be5b-c34d2e2b738e',
 			},
 		],
 	},
